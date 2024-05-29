35bf813218b73883056027ba712f9ce29238aba4
Sushant Sharma <sushant.wurk@gmail.com>
2024-05-29 22:56:09 +0530

    Chore: remove unused

--- src/feature/LibraryContext.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { createContext } from "react";
-
-const LibraryContext = createContext<boolean>(false);
-
-function LibraryProvider({ children }: JSX.Element) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  return <LibraryContext.Provider value={}>{children}</LibraryContext.Provider>;
-}
